f418bd54d0a0cfca04cf1fe47b402bf5a60371c4
Stefan Krumov <57665732+StefanKrumov@users.noreply.github.com>
2020-03-27 13:51:29 +0200

    added limit to misclicks

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ function pageHandler() {
   const message = document.querySelector(".secret-message");
   const hideArea = document.querySelector(".button-hide-area");
   const targetButton = document.getElementById("button-target");
+  const hideAreaWrapper = document.querySelector(".button-area-wrapper");
+  const hideAreaTitle = document.querySelector(".button-area-title");
   targetButton.style.opacity = "0";
+  let clicksMissed = 0;
+  targetButton.style.top = `${Math.floor(Math.random() * 90)}%`;
+  targetButton.style.left = `${Math.floor(Math.random() * 90)}%`;
 
-  function clickHandler(timeoutID) {
+  function targetClickHandler(timeoutID) {
     hideArea.removeEventListener(`mousemove`, hoverHandler);
-    hideArea.style.display = "none";
+    hideAreaWrapper.style.display = "none";
     message.style.display = "block";
     targetButton.innerText = "SUCCESS";
     targetButton.style.background = "radial-gradient(#44ff44, #023801)";
@@ -22,11 +27,27 @@ function pageHandler() {
       e.target.style.opacity = "0";
       e.target.style.top = `${Math.floor(Math.random() * 90)}%`;
       e.target.style.left = `${Math.floor(Math.random() * 90)}%`;
-    }, 1000);
-    e.target.addEventListener("click", clickHandler.bind(undefined, timeoutID));
+    }, 550);
+    e.target.addEventListener(
+      "click",
+      targetClickHandler.bind(undefined, timeoutID)
+    );
+  }
+
+  function hideAreaClickHandler(e) {
+    if (e.target.id === "button-target") {
+      return;
+    }
+    clicksMissed++;
+    if (clicksMissed >= 3) {
+      hideArea.style.display = "none";
+      hideAreaTitle.innerText = "MISSION FAILED!";
+      hideAreaTitle.style.color = "red";
+    }
   }
 
   hideArea.addEventListener(`mousemove`, hoverHandler);
+  hideArea.addEventListener(`click`, hideAreaClickHandler);
 }
 
 document.addEventListener("DOMContentLoaded", pageHandler);
